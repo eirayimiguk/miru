cf2edc2e84e415e531a390b5a70f99528d65c43b
sistersatori <sistersatori@gmail.com>
2022-11-03 22:42:21 +0900

    update

--- miru/__main__.py
+++ miru/__main__.py
@@ -38,26 +38,12 @@ async def index(request: Request):
     )
 
 
-# @app.route('/', methods=["GET", "POST"])
-# def index():
-#     if request.method == "GET":
-#         return render_template("index.html")
-#     if request.method == "POST":
-#         tags = request.form["tags"].split(", ")
-#         urls = search_images(tags)
-#         return render_template("index.html", urls=urls)
-#
-#
-# @app.route('/update')
-# def update():
-#     update_tags()
-#     return render_template("update.html")
-#
-#
-# @app.route('/tags')
-# def tags():
-#     tags = get_tags()
-#     return render_template("tags.html", tags=tags)
+@app.get('/tags')
+def tags(request: Request):
+    tags = get_tags()
+    return templates.TemplateResponse(
+        "tags.html", {"request": request, "tags": tags}
+    )
 
 
 if __name__ == "__main__":
