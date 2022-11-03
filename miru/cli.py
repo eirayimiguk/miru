7117933a9b5b913264d588ec6b88a3e7b62866ad
sistersatori <sistersatori@gmail.com>
2022-11-03 13:04:19 +0900

    呪文ランダム生成

--- miru/cli.py
+++ miru/cli.py
@@ -1,9 +1,20 @@
-from miru.batch import get_words
+import random
+
+from miru.batch import get_tags
+
+
+def chanting_magic_at_random(length: int = 10):
+    tags = get_tags()
+
+    spell = []
+    for _ in range(length):
+        index = random.randint(0, len(tags) - 1)
+        spell.append(tags[index]["name"])
+    print(", ".join(spell))
 
 
 def main():
-    words = get_words()
-    print(words)
+    chanting_magic_at_random(50)
 
 
 if __name__ == "__main__":
