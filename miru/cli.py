882a632ff4c48f2197f2fab9936dba511830a8ab
sistersatori <sistersatori@gmail.com>
2022-11-05 13:17:49 +0900

    update

--- miru/cli.py
+++ miru/cli.py
@@ -6,6 +6,7 @@ from optparse import OptionParser
 import requests
 import climage
 
+from exif import Image
 from prompt_toolkit import prompt
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
@@ -18,34 +19,59 @@ def status_line():
     return "To exit Ctrl+C"
 
 
+def update(opts: list = None):
+    update_tags()
+
+
+def search(opts: list = None):
+    urls = []
+    if len(opts) > 1:
+        urls, _ = search_images(opts, page_size=1)
+    else:
+        urls, _ = search_images(page_size=1)
+
+    for url in urls:
+        output_image_to_terminal(url)
+
+
+def random_chant(opts: list = None):
+    if len(opts) > 1:
+        length = int(cmd[1])
+    else:
+        length = 30
+    chanting_magic_at_random(length)
+
+
+def output_image_to_terminal(url: str):
+    r = requests.get(url)
+    with open("cache/image.png", "wb") as f:
+        f.write(r.content)
+    img = climage.convert(
+        "cache/image.png",
+        is_unicode=True,
+        is_256color=True,
+        is_truecolor=False,
+        width=100)
+    print(img)
+
+
 def main():
     try:
         while True:
-            cmd = prompt(">>> ",
+            cmdline = prompt(">>> ",
                 bottom_toolbar=status_line,
                 history=FileHistory("cache/history"),
                 auto_suggest=AutoSuggestFromHistory()).split(" ")
 
-            if cmd[0] == "update":
-                update_tags()
-            elif cmd[0] == "search":
-                urls = []
-                if len(cmd) > 1:
-                    urls, next_cursor = search_images(cmd[1:], page_size=3)
-                else:
-                    urls, next_cursor = search_images(page_size=3)
-                for url in urls:
-                    r = requests.get(url)
-                    with open("cache/image.png", "wb") as img:
-                        img.write(r.content)
-                        o = climage.convert("cache/image.png", is_256color=False, is_truecolor=True, width=128)
-                        print(o)
-            elif cmd[0] == "random":
-                if len(cmd) > 1:
-                    length = int(cmd[1])
-                else:
-                    length = 30
-                chanting_magic_at_random(length)
+            cmd = cmdline[0]
+            opts = cmdline[1:]
+
+            if cmd == "update":
+                update(opts)
+            elif cmd == "search":
+                search(opts)
+            elif cmd == "random":
+                random_chant(opts)
     except KeyboardInterrupt:
         pass
 
